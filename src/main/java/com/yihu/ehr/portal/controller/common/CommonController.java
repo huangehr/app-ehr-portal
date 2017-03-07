ec49eaf0bdc8d782c596fbbae93a3312b2a0d99c
huangzhiyong <944439284@qq.com>
2017-03-07 17:23:33 +0800

    冲突姐姐

--- src/main/java/com/yihu/ehr/portal/controller/common/CommonController.java
+++ src/main/java/com/yihu/ehr/portal/controller/common/CommonController.java
@@ -5,15 +5,12 @@ import com.yihu.ehr.portal.model.Result;
 import com.yihu.ehr.portal.service.common.BaseService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 
