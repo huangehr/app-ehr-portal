1ee802639099c592936b852f213059b6d6824b0a
Progr1mmer <653800209@qq.com>
2017-09-18 17:24:02 +0800

    内外网IP判断

--- src/main/java/com/yihu/ehr/portal/service/common/OauthService.java
+++ src/main/java/com/yihu/ehr/portal/service/common/OauthService.java
@@ -25,16 +25,12 @@ import java.util.Map;
 @Service
 public class OauthService extends BaseService {
 
-
     @Autowired
-    ObjectMapper objectMapper;
-
+    private ObjectMapper objectMapper;
     @Value("${app.oauth2authorize}")
     public String authorize;
-
     @Value("${service-gateway.portalUrl}")
     public String portalUrl;
-
     @Value("${app.clientId}")
     public String clientId;
 
@@ -43,19 +39,16 @@ public class OauthService extends BaseService {
      */
     public Result login(HttpServletRequest request, String userName, String password) {
         try {
-
             ObjectResult result = new ObjectResult();
-
             Map<String, Object> params = new HashMap<>();
             params.put("userName", userName);
             params.put("password", password);
             params.put("clientId", clientId);
-
             HttpResponse response = HttpHelper.get(portalUrl + "/oauth/login", params);
-
             if (response!=null && response.getStatusCode() == 200) {
                 ObjectResult re = toModel(response.getBody(), ObjectResult.class);
                 if (re.isSuccessFlg()){
+                    initUrl(request);
                     Map userMap = new HashMap<>();
                     userMap.put("user",re.getData());
                     result.setData(userMap);
@@ -69,11 +62,9 @@ public class OauthService extends BaseService {
                         request.getSession().setAttribute("token", token);
                         request.getSession().setAttribute("loginName", userName);
                         request.getSession().setAttribute("userId", userId);
-
                         result.setSuccessFlg(true);
                         result.setMessage("登录成功");
                         result.setCode(200);
-
                         return result;
                     }
                     else{
@@ -91,6 +82,63 @@ public class OauthService extends BaseService {
             e.printStackTrace();
             return Result.error(e.getMessage());
         }
+    }
+
+    public static final long a1 = getIpNum("10.0.0.0");
+    public static final long a2 = getIpNum("10.255.255.255");
+    public static final long b1 = getIpNum("172.16.0.0");
+    public static final long b2 = getIpNum("172.31.255.255");
+    public static final long c1 = getIpNum("192.168.0.0");
+    public static final long c2 = getIpNum("192.168.255.255");
+    public static final long d1 = getIpNum("10.44.0.0");
+    public static final long d2 = getIpNum("10.69.0.255");
+
+    public void initUrl(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if(ip != null) {
+            if("0:0:0:0:0:0:0:1".equals(ip)) {
+                System.out.println("ip:" + ip);
+                System.out.println("localhost");
+                return;
+            }else {
+                if("127.0.0.1".equals(ip) || isInnerIP(ip)) {
+                    System.out.println("ip:" + ip);
+                    System.out.println("innerIP");
+                }else {
+                    System.out.println("ip:" + ip);
+                    System.out.println("outerIp");
+                }
+            }
+        }
+    }
+
+    private static long getIpNum(String ipAddress) {
+        String [] ip = ipAddress.split("\\.");
+        long a = Integer.parseInt(ip[0]);
+        long b = Integer.parseInt(ip[1]);
+        long c = Integer.parseInt(ip[2]);
+        long d = Integer.parseInt(ip[3]);
+        return a * 256 * 256 * 256 + b * 256 * 256 + c * 256 + d;
+    }
+
+    public static boolean isInnerIP(String ip){
+        long n = getIpNum(ip);
+        return (n >= a1 && n <= a2) || (n >= b1 && n <= b2) || (n >= c1 && n <= c2) || (n >= d1 && n <= d2);
     }
 
     /**
@@ -183,4 +231,5 @@ public class OauthService extends BaseService {
         request.getSession().removeAttribute("userId");
         response.sendRedirect("/login");
     }
+
 }
