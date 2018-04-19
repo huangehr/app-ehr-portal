7f17d85eb619552836a97c7c298e87f1e59eb6cd
Progr1mmer <653800209@qq.com>
2018-04-19 19:05:20 +0800

    去掉无用配置

--- src/main/java/com/yihu/ehr/portal/service/common/BaseService.java
+++ src/main/java/com/yihu/ehr/portal/service/common/BaseService.java
@@ -31,8 +31,6 @@ public class BaseService {
     protected String clientId;
     @Value("${service-gateway.profileInnerUrl}")
     protected String profileInnerUrl;
-    @Value("${service-gateway.profileOuterUrl}")
-    protected String profileOuterUrl;
     @Value("${app.oauth2InnerUrl}")
     protected String oauth2InnerUrl;
     @Value("${app.oauth2OuterUrl}")
