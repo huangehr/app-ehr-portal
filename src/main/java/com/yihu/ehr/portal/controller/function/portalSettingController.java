76e297801eb7309ce4bd24c22a06caaa57ebd76d
zdm <763558454@qq.com>
2018-05-17 18:44:26 +0800

    获取配置logo已底部主办方接口

--- src/main/java/com/yihu/ehr/portal/controller/function/portalSettingController.java
+++ src/main/java/com/yihu/ehr/portal/controller/function/portalSettingController.java
@@ -2,15 +2,16 @@ package com.yihu.ehr.portal.controller.function;
 
 import com.yihu.ehr.portal.model.Result;
 import com.yihu.ehr.portal.service.function.PortalSettingService;
+import com.yihu.ehr.util.rest.Envelop;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
 
 /**
  * @author HZY
@@ -23,7 +24,6 @@ public class portalSettingController {
 
     @Autowired
     private PortalSettingService portalSettingService;
-
     @RequestMapping(value = "portalSetting",method = RequestMethod.GET)
     public String login(Model model) {
         model.addAttribute("title","门户配置");
@@ -37,6 +37,17 @@ public class portalSettingController {
     @ApiOperation(value = "门户配置信息列表的获取", produces = "application/json", notes = "门户配置信息列表的获取")
     public Result infoData() throws Exception {
         return portalSettingService.getPortalSettingList();
+    }
+
+
+    @RequestMapping(value = "portalSetting/getLogoByDictAndEntryCode",method = RequestMethod.GET)
+    @ApiOperation(value = "云门户/基础支撑LOGO获取")
+    public Envelop getLogoByDictAndEntryCode(
+             @ApiParam(name = "dictEntryCode", value = "字典项编码", required = true)
+             @RequestParam(value = "dictEntryCode", required = true) String dictEntryCode,
+             @ApiParam(name = "type", value = "1代表获取logo，2代表主办方文字", required = true)
+             @RequestParam(value = "type", required = true) String type) throws Exception{
+        return portalSettingService.getLogoByDictAndEntryCode(dictEntryCode,type);
     }
 
 }
--- src/main/java/com/yihu/ehr/portal/service/function/PortalSettingService.java
+++ src/main/java/com/yihu/ehr/portal/service/function/PortalSettingService.java
@@ -1,14 +1,17 @@
 package com.yihu.ehr.portal.service.function;
 
+import com.yihu.ehr.agModel.dict.SystemDictEntryModel;
 import com.yihu.ehr.portal.model.ListResult;
 import com.yihu.ehr.portal.model.Result;
 import com.yihu.ehr.portal.service.common.BaseService;
 import com.yihu.ehr.util.http.HttpResponse;
 import com.yihu.ehr.util.http.HttpUtils;
+import com.yihu.ehr.util.rest.Envelop;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author HZY
@@ -17,6 +20,8 @@ import java.util.Map;
  */
 @Service
 public class PortalSettingService extends BaseService {
+    @Value("${service-gateway.adminInnerUrl}")
+    private String adminInnerUrl;
 
     /**
      * 获取门户配置信息
@@ -33,5 +38,52 @@ public class PortalSettingService extends BaseService {
             return Result.error("门户配置-数据接口请求失败");
         }
     }
+
+    /**
+     * 获取门户LOGO配置信息
+     * @return
+     */
+    public Envelop getLogoByDictAndEntryCode(String dictEntryCode,String type) throws Exception {
+        Envelop envelop = new Envelop();
+        Map<String, Object> params = new HashMap<>();
+        String url="/resource/api/v1.0/resources/system_dict_entries";
+        StringBuffer filters = new StringBuffer();
+        filters.append("dictCode=" + 125 + ";");
+        if (!StringUtils.isEmpty(dictEntryCode)) {
+            filters.append("code=" + dictEntryCode + ";");
+        }
+        params.put("filters", filters.toString());
+        params.put("fields", "");
+        params.put("sorts", 1);
+        params.put("page", 1);
+        params.put("size", 10);
+        HttpResponse response = HttpUtils.doGet(adminInnerUrl + url,params);
+        if (response.isSuccessFlg()) {
+            envelop = toModel(response.getContent(), Envelop.class);
+            if(type.equals("1")){
+                if(null != envelop && null!=envelop.getDetailModelList() && envelop.getDetailModelList().size()>0){
+                    LinkedHashMap item = (LinkedHashMap) envelop.getDetailModelList().get(0);
+                    SystemDictEntryModel dict = (SystemDictEntryModel) toModel(toJson(item), SystemDictEntryModel.class);
+                    //根据图片名称 获取图片路径
+                    url="/dfs/api/v1.0/fastDfs/page";
+                    params = new HashMap<>();
+                    filters = new StringBuffer();
+                    filters.append("[{\"andOr\":\"and\",\"condition\":\"=\",\"field\":\"name\",\"value\":\""+dict.getValue()+"\"}]" + ";");
+                    params.put("filters", filters.toString());
+                    params.put("page", 1);
+                    params.put("size", 10);
+                    response = HttpUtils.doGet(adminInnerUrl + url,params);
+                    envelop = toModel(response.getContent(), Envelop.class);
+                }
+            }
+            return envelop;
+        } else {
+            envelop.setSuccessFlg(false);
+            return envelop;
+        }
+    }
+
+
+
 
 }
