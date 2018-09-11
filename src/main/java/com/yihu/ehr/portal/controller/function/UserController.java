357308814722be006aa453b59d97a9dca3abe9ea
zdm <763558454@qq.com>
2018-09-11 15:12:15 +0800

    用户头像获取

--- src/main/java/com/yihu/ehr/portal/controller/function/UserController.java
+++ src/main/java/com/yihu/ehr/portal/controller/function/UserController.java
@@ -1,17 +1,27 @@
 package com.yihu.ehr.portal.controller.function;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yihu.ehr.portal.model.Result;
 import com.yihu.ehr.portal.service.function.AppService;
+import com.yihu.ehr.util.http.HttpResponse;
+import com.yihu.ehr.util.http.HttpUtils;
 import com.yihu.ehr.util.rest.Envelop;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -24,6 +34,12 @@ public class UserController {
 
     @Autowired
     private AppService appService;
+
+    @Value("${service-gateway.adminInnerUrl}")
+    private String adminInnerUrl;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
 
     /******************************** 页面 **********************************/
 
@@ -66,6 +82,20 @@ public class UserController {
     @ResponseBody
     public Envelop getUserRoleApps(String userId) throws Exception {
         return appService.getUserRoleApp(userId);
+    }
+
+    @RequestMapping("showImage")
+    @ResponseBody
+    public String showImage(String imgRemotePath, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String res = "";
+        if (!StringUtils.isEmpty(imgRemotePath)) {
+            Map<String, Object> params = new HashMap<>();
+            params.put("fileId", imgRemotePath);
+            HttpResponse resp = HttpUtils.doGet(adminInnerUrl + "/basic/api/v1.0/file/getFileStorage", params);
+            res = objectMapper.readValue(resp.getContent(), Envelop.class).getObj().toString();
+
+        }
+        return res;
     }
 
 
